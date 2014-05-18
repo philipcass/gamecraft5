09ef5f3c6dec73bd58fca9d5654be728b4f187b8
philipcass <philipmcass@gmail.com>
2014-05-18 04:05:04 +0100

    stuff

---
+++ Assets/Scripts/Scorebar.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Scorebar : MonoBehaviour {
+	public float percentage;
+	public Allegiance myAllegiance;
+	public LeaderConfig conf;
+	public int height;
+	// Use this for initialization
+	Vector2 p;
+	void Start () {
+		p = this.transform.position;
+		this.GetComponentInChildren<BannerSelector> ().SetBanner (conf.CrestSprite[Array.IndexOf(conf.Allegiance, this.myAllegiance)]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = Vector2.Lerp (this.transform.position, p + new Vector2 (0, height)*percentage, Time.deltaTime);
+	}
+}
